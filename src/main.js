f82323f42f86b704e4e56a05c01dc84deb0b235a
Perla <pataditaenloshuevos@gmail.com>
2019-02-19 09:30:51 -0600

    registro de usuario

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//variable de bienvenida.
+const buttonUseRegister=document.getElementById('buttonRegister');
+//Funcion para el registro.
+buttonUseRegister.addEventListener('click',function(){
+//variable de input para registrar usuario.
+const userInput =document.getElementById('user').value;
+document.getElementById('useRegister').classList.add('display_none');
+document.getElementById('useRegister').classList.remove('display_block');
+
+document.getElementById('hi').innerHTML = 'Hola '+userInput;
+});
