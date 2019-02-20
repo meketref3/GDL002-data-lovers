df129a8db7c8a77dcf1ea18b18c813e8dfe68546
Perla <pataditaenloshuevos@gmail.com>
2019-02-20 09:21:16 -0600

    sin errores pero aun no funciona

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
 //variable para obtener los datos de mi pokemon.js
 const dataPokemon = window.POKEMON.pokemon;
 
-//creo mi funcion para ver las imagenes en mi root
+//creo mi funcion para ver las imagenes.
 const showImagePokemon =(dataPokemon)=>{
-  const result='';
+
+  let result=document.getElementById('card').innerHTML;
+
   let data=dataPokemon.pokemon;
 
   data.filter(element=>{
-    //document.write(element.img);
-    console.log(element.img);
-   //document.getElementById('imgPokemon').innerHTML + element.img;
+    //console.log(element.img);
+   result = result + element.img;
   });
-  //return document.getElementById('imgPokemon').innerHTML= element.img;
+  return result;
+  console.log(result);
 };
+//este window es para que me muestre lo que le estoy pidiendo en mi showImagePokemon.
 window.open = showImagePokemon(POKEMON);
+
 
 
 //variable de bienvenida.
