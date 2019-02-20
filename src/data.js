fceeacbbe62a1d5425bfe64c3d319a04a0478b8f
Perla <pataditaenloshuevos@gmail.com>
2019-02-20 16:53:52 -0600

    imagenes prueba 2

--- src/data.js
+++ src/data.js
@@ -1,8 +1,32 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
+};*/
+//onst buttonUseRegister=document.getElementById('buttonRegister');
+
+//const infoPokemon = window.POKEMON.pokemon;
+
+//let list=[];
+document.getElementById('buttonRegister').addEventListener('click', showPokemons);
+function fillElement (infoPokemon, activeElem){
+
+    for(let i=0; i<infoPokemon.length; i++){
+
+         let newDiv=document.createElement("div");
+         newDiv.className ="pictures";
+         newDiv.innerHTML = "<img src=\"" +infoPokemon[i].img+ "\">";
+         activeElem.insertAdjacentElement("beforeend", newDiv);
+         
+       };
+    };
+
+function showPokemons(){
+  let infoPokemon = POKEMON.pokemon;
+  let divPokemonList = document.getElementById("contenedor");
+  fillElement(infoPokemon,divPokemonList);
 };
 
-window.example = example;
+
+//console.log(fillElement);
