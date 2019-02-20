27a4c0f9b338cf1be261bde6dda535b08d5fed2c
Perla <pataditaenloshuevos@gmail.com>
2019-02-20 08:31:50 -0600

    prueba 1 imagen

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+//variable para obtener los datos de mi pokemon.js
+const dataPokemon = window.POKEMON.pokemon;
+
+//creo mi funcion para ver las imagenes en mi root
+const showImagePokemon =(dataPokemon)=>{
+  const result='';
+  let data=dataPokemon.pokemon;
+
+  data.filter(element=>{
+    //document.write(element.img);
+    console.log(element.img);
+   //document.getElementById('imgPokemon').innerHTML + element.img;
+  });
+  //return document.getElementById('imgPokemon').innerHTML= element.img;
+};
+window.open = showImagePokemon(POKEMON);
+
+
 //variable de bienvenida.
 const buttonUseRegister=document.getElementById('buttonRegister');
 //Funcion para el registro.
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,7 +1,31 @@
 require('../src/data.js');
+const pokemon = require('../src/data/pokemon/pokemon.js');
 
+describe('pokemon',()=>{
+  it('is an object',()=>{
+    expect(typeof pokemon).toBe('object');
+  });
+});
 
-describe('example', () => {
+describe('filterData',()=>{
+  it('is a function',()=>{
+    expect(typeof filterData).toBe('function');
+  });
+});
+
+describe('sortData',()=>{
+  it('is a function',()=>{
+    expect(typeof sortData).toBe('function');
+  });
+});
+
+describe('computeStats',()=>{
+  it('is a function',()=>{
+    expect(typeof computeStats).toBe('function');
+  });
+});
+
+/*describe('example', () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
   });
@@ -9,4 +33,4 @@ describe('example', () => {
   it('returns `example`', () => {
     expect(example()).toBe('example');
   });
-});
+});*/
